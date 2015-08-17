0c4b6bc73d3733a4d81506c4a9a7861f20c76f8f
gehlotchirag <gehlotchirag1988@gmail.com>
2015-08-17 17:51:16 +0530

    emailer added

--- public/modules/doctors/controllers/doctors.client.controller.js
+++ public/modules/doctors/controllers/doctors.client.controller.js
@@ -419,7 +419,9 @@ angular.module('doctors').controller('DoctorsController', ['$scope', '$http' ,'$
         var data = ({
           to: to,
           subject: 'TechWiss - Video Consulting Initiation Link',
-          html: "<h1> TechWiss - Video Consulting invite</h1><p>Hello</p><p>Your TechWiss Video Consulting Link is <a href = 'window.location.host+'/appointments/' + appointmentId'> window.location.host+'/appointments/' + appointmentId </p>"
+          html: "<b>Hello world</b>" // html body
+
+          //html: "<h1> TechWiss - Video Consulting invite</h1><p>Hello</p><p>Your TechWiss Video Consulting Link is <a href = 'window.location.host+'/appointments/' + appointmentId'> window.location.host+'/appointments/' + appointmentId </p>"
         });
 
         $http.post('/appointment-email', data).
