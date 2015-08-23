412a665ba3aa88ed2618df4e297116bdc2c9373a
gehlotchirag <gehlotchirag1988@gmail.com>
2015-08-23 18:26:45 +0530

    Map for Visiting Doctors

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -73,7 +73,7 @@ exports.getS3Policy = function(req, res) {
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'gehlotchirag1988@gmail.com',
+    user: 'gehlotchirag777@gmail.com',
     pass: 'Gandhi007.'
   }
 });
@@ -91,7 +91,7 @@ exports.index = function(req, res) {
 exports.sendEmail = function(req, res) {
   var data = req.body;
   var mailOptions = {
-    from: 'TechWiss Team <gehlotchirag1988@gmail.com>',
+    from: 'TechWiss Team <gehlotchirag777@gmail.com>',
     to: data.to,
     subject: data.subject,
     text: data.text
